3081d9240cdec5af64eebd3e68315d01328ec282
kaoudis <reverseg985@gmail.com>
2025-07-17 18:03:22 +0300

    Added some cool animations and also implemented sticky while it still has some problems

---
+++ TODO.md
@@ -0,0 +1,4 @@
+## 1. Do the Greek html version and make it to be able to change languages
+## 2. figure out why when the element is sticky the eventlisteners dont work
+## 3. add some kind of animation when the user changes languages
+
--- docs/scripts.js
+++ docs/scripts.js
@@ -1,6 +1,16 @@
 // Handles all interactivity of the web page
 // Created: 16/07/2025
 
+// smooth transaction for dropdown
+function transDrop(dropdown){
+    // the class attribute needs to be removes so the animation can
+    // play again.
+    dropdown.hidden = false;
+    dropdown.classList.remove("animate-fade-in");
+    void dropdown.offsetWidth;
+    dropdown.className = "animate-fade-in";
+}
+
 // Salads & Starters
 function handleSalads(){
     const saladList = document.getElementById("SaladsList");
@@ -8,14 +18,25 @@ function handleSalads(){
 
     if (saladList.hidden === true){
         saladList.hidden = false;
+        dropS.hidden = true;
         dropS.srcset = "assets/dropup.svg";
         dropS.src = "assets/dropup.png";
         dropS.alt = "dropup";
+        transDrop(dropS);
+        // i could have used classList.add for this but i didnt know its
+        // existence and i dont want to change it in case it breaks!
+        saladList.className = "flex flex-col items-center space-y-20 animate-fade-in-down"
     }else{
-        saladList.hidden = true;
+        saladList.className = "flex flex-col items-center space-y-20 animate-fade-out-up animate-duration-400"
+        setTimeout(() => {
+            saladList.hidden = true;
+        }, 400);
+
+        dropS.hidden = true;
         dropS.srcset = "assets/dropdown.svg";
         dropS.src = "assets/dropdown.png";
         dropS.alt = "dropdown";
+        transDrop(dropS);
     }
 }
 
@@ -26,16 +47,26 @@ function handleDishes(){
 
     if (dishesList.hidden === true){
         dishesList.hidden = false;
+        dropD.hidden = true;
         dropD.srcset = "assets/dropup.svg";
         dropD.src = "assets/dropup.png";
         dropD.alt = "dropup";
+        transDrop(dropD);
+        dishesList.className = "flex flex-col items-center space-y-20 animate-fade-in-down"
     }else{
-        dishesList.hidden = true;
+        // animations
+        dishesList.className = "flex flex-col items-center space-y-20 animate-fade-out-up animete-duration-400"
+        setTimeout(() => {
+            dishesList.hidden = true;
+        }, 400);
+
+        dropD.hidden = true;
         dropD.srcset = "assets/dropdown.svg";
         dropD.src = "assets/dropdown.png";
         dropD.alt = "dropdown";
-    }
+        transDrop(dropD);
 
+    }
 }
 
 // Mixed Plates
@@ -45,16 +76,28 @@ function handleMixed(){
 
     if (mixedList.hidden === true){
         mixedList.hidden = false;
+        dropM.hidden = true;
         dropM.srcset = "assets/dropup.svg";
         dropM.src = "assets/dropup.png";
         dropM.alt = "dropup";
+        transDrop(dropM);
+        mixedList.className = "flex flex-col items-center space-y-20 animate-fade-in-down"
     }else{
-        mixedList.hidden = true;
+        // animations
+        mixedList.className = "flex flex-col items-center space-y-20 animate-fade-out-up animete-duration-400"
+        setTimeout(() => {
+            mixedList.hidden = true;
+            dropM.srcset = "assets/dropdown.svg";
+            dropM.src = "assets/dropdown.png";
+            dropM.alt = "dropdown";
+        }, 400);
+
+        dropM.hidden = true;
         dropM.srcset = "assets/dropdown.svg";
         dropM.src = "assets/dropdown.png";
         dropM.alt = "dropdown";
+        transDrop(dropM);
     }
-
 }
 
 // Souvlaki Pita
@@ -64,16 +107,25 @@ function handleSouvlaki(){
 
     if (souvlakiList.hidden === true){
         souvlakiList.hidden = false;
+        dropS.hidden = true;
         dropS.srcset = "assets/dropup.svg";
         dropS.src = "assets/dropup.png";
         dropS.alt = "dropup";
+        transDrop(dropS);
+        souvlakiList.className = "flex flex-col items-center space-y-20 animate-fade-in-down"
     }else{
-        souvlakiList.hidden = true;
+        // animations
+        souvlakiList.className = "flex flex-col items-center space-y-20 animate-fade-out-up animete-duration-400"
+        setTimeout(() => {
+            souvlakiList.hidden = true;
+        }, 400);
+
+        dropS.hidden = true;
         dropS.srcset = "assets/dropdown.svg";
         dropS.src = "assets/dropdown.png";
         dropS.alt = "dropdown";
+        transDrop(dropS);
     }
-
 }
 
 // Drinks
@@ -81,19 +133,30 @@ function handleDrinks(){
     const drinksList = document.getElementById("DrinksList");
     const dropDr = document.getElementById("dropDrinks");
 
-
     if (drinksList.hidden === true){
         drinksList.hidden = false;
+        dropDr.hidden = true;
         dropDr.srcset = "assets/dropup.svg";
         dropDr.src = "assets/dropup.png";
         dropDr.alt = "dropup";
+        transDrop(dropDr);
+        drinksList.className = "flex flex-col items-center space-y-20 animate-fade-in-down"
     }else{
-        drinksList.hidden = true;
+        // animations
+        drinksList.className = "flex flex-col items-center space-y-20 animate-fade-out-up animete-duration-400"
+        setTimeout(() => {
+            drinksList.hidden = true;
+            dropDr.srcset = "assets/dropdown.svg";
+            dropDr.src = "assets/dropdown.png";
+            dropDr.alt = "dropdown";
+        }, 400);
+
+        dropDr.hidden = true;
         dropDr.srcset = "assets/dropdown.svg";
         dropDr.src = "assets/dropdown.png";
         dropDr.alt = "dropdown";
+        transDrop(dropDr);
     }
-
 }
 
 // event listeners
